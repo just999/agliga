72d0eeb82eb638895bae5e0b1c3658e69ac8eca3
tommy88888 <tommydewa08@gmail.com>
2024-06-01 18:21:10 +0700

    test new ubuntu

--- app/(sitemap)/sitemap.ts
+++ app/(sitemap)/sitemap.ts
@@ -1,35 +1,35 @@
-import { getPosts } from '@/lib/queries/posts';
+// import { getPosts } from '@/lib/queries/posts';
 
-import { MetadataRoute } from 'next';
+// import { MetadataRoute } from 'next';
 
-const website_host_url = process.env.NEXT_PUBLIC_DOMAIN;
+// const website_host_url = process.env.NEXT_PUBLIC_DOMAIN;
 
-type changeFrequency =
-  | 'yearly'
-  | 'monthly'
-  | 'weekly'
-  | 'daily'
-  | 'hourly'
-  | 'always'
-  | 'never';
+// type changeFrequency =
+//   | 'yearly'
+//   | 'monthly'
+//   | 'weekly'
+//   | 'daily'
+//   | 'hourly'
+//   | 'always'
+//   | 'never';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const news = await getPosts();
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const news = await getPosts();
 
-  const changeFrequency = 'daily' as changeFrequency;
-  if (!news) return [];
+//   const changeFrequency = 'daily' as changeFrequency;
+//   if (!news) return [];
 
-  const posts = news?.map(({ id, createdAt }) => ({
-    url: `${website_host_url}/posts/${id}`,
-    lastModified: createdAt.toISOString(),
-    changeFrequency,
-  }));
+//   const posts = news?.map(({ id, createdAt }) => ({
+//     url: `${website_host_url}/posts/${id}`,
+//     lastModified: createdAt.toISOString(),
+//     changeFrequency,
+//   }));
 
-  const routes = ['', '/about', '/posts', '/soccer'].map((route) => ({
-    url: `${website_host_url}${route}`,
-    lastModified: new Date().toISOString(),
-    changeFrequency,
-  }));
+//   const routes = ['', '/about', '/posts', '/soccer'].map((route) => ({
+//     url: `${website_host_url}${route}`,
+//     lastModified: new Date().toISOString(),
+//     changeFrequency,
+//   }));
 
-  return [...routes, ...(posts || [])];
-}
+//   return [...routes, ...(posts || [])];
+// }
