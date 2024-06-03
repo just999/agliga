1f8169d27014205f2412f16345f5df4b64f48024
tommy88888 <tommydewa08@gmail.com>
2024-06-03 13:26:23 +0700

    test new solution ci2

--- app/(sitemap)/robots.ts
+++ app/(sitemap)/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private',
     },
-    sitemap: 'http://localhost:3000/sitemap.xml',
+    sitemap: 'http://181.215.69.147:3000/sitemap.xml',
   };
 }
